8281bc9aa4fff1ebaec9a0a27f8e91de9d392962
Alberto <albertoperuchi11@gmail.com>
2024-11-13 21:17:50 -0300

    Aprimorando as operações de CRUD

--- src/main/java/com/fiap/sunwise/controller/ClienteController.java
+++ src/main/java/com/fiap/sunwise/controller/ClienteController.java
@@ -2,6 +2,8 @@ package com.fiap.sunwise.controller;
 
 import com.fiap.sunwise.model.Cliente;
 import com.fiap.sunwise.repository.ClienteRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,30 +14,35 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/clientes")
+@RequestMapping("/client")
+@Tag(name = "Clientes", description = "Endpoint relacionado com os projetos dos clientes SunWise")
 public class ClienteController {
 
     @Autowired
     private ClienteRepository clienteRepository;
 
     @PostMapping
+    @Operation(summary = "Cadastra um cliente no sistema.")
     public ResponseEntity<Cliente> criarCliente(@Valid @RequestBody Cliente cliente) {
         Cliente clienteCriado = clienteRepository.save(cliente);
         return new ResponseEntity<>(clienteCriado, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Busca um cliente pelo id.")
     public ResponseEntity<Cliente> buscarClientePorId(@PathVariable Long id) {
         Optional<Cliente> cliente = clienteRepository.findById(id);
         return cliente.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping
+    @Operation(summary = "Lista todos os clientes cadastrados no sistema.")
     public List<Cliente> listarTodosClientes() {
         return clienteRepository.findAll();
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Atualiza os dados de um cliente com base no id.")
     public ResponseEntity<Cliente> atualizarCliente(@PathVariable Long id, @Valid @RequestBody Cliente clienteAtualizado) {
         if (!clienteRepository.existsById(id)) {
             return ResponseEntity.notFound().build();
@@ -46,6 +53,7 @@ public class ClienteController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Apaga um cliente do sistema.")
     public ResponseEntity<Void> excluirCliente(@PathVariable Long id) {
         if (!clienteRepository.existsById(id)) {
             return ResponseEntity.notFound().build();
